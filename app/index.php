6bdea74eb74e95777c799416e04ca59d372d2f04
Davide Madrisan <davide.madrisan@gmail.com>
2013-03-24 23:49:41 +0100

    reindent source code
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- app/index.php
+++ app/index.php
@@ -1,5 +1,4 @@
 <?php
-//session_start();
 
 if(isset($_POST['setup']))
 {
@@ -16,8 +15,8 @@ if(isset($_POST['setup']))
     $myFile = "/etc/raspcontrol/database.aptmnt";
     if(!file_exists($myFile))
     {
-      require('_lib/includes/_header.php');
-      ?>
+        require('_lib/includes/_header.php');
+?>
         <div id="firstBlockContainer">
           <div class="firstBlockWrapper">
             <div style="padding-top: 20px;">
@@ -35,13 +34,13 @@ if(isset($_POST['setup']))
           </div>
           <br/><br/><br/>
         </div>
-      <?php
-      require('_lib/includes/_footer.php'); 
-      die();
+<?php
+        require('_lib/includes/_footer.php'); 
+        die();
     }
     else
     {
-      $fh = fopen($myFile, 'w');
+        $fh = fopen($myFile, 'w');
     }
 
     // Bake that Pi
@@ -54,78 +53,81 @@ if(isset($_POST['setup']))
     // Eat it
     header('location: index.php');
 
-    } else {
+    }
+    else
+    {
+        $filename = '/etc/raspcontrol/database.aptmnt';
 
+        if (file_exists($filename))
+        {
+            session_start();
 
-	$filename = '/etc/raspcontrol/database.aptmnt';
+            if($_SESSION['username'] != "")
+            {
+                require('main.php'); 
+                die;
+            }
 
-	if (file_exists($filename)) {
-		session_start();
-
-		if($_SESSION['username'] != "")
-                {
-			require('main.php'); 
-			die;
-		}
-
-		require('_lib/includes/_header.php');
-		require('_lib/classes/_login.php'); 
+            require('_lib/includes/_header.php');
+            require('_lib/classes/_login.php'); 
 ?>
 
-	    <div id="firstBlockContainer">
-	        <div class="firstBlockWrapper">
-	        	
-	        	<div style="padding-top: 20px;">
-	        	<center>
-	        		Please login to Raspcontrol!<br/><br/>
-		        	<form name="login" method="post" action="index.php">
-		        		<input type="text" name="username" class="loginForm" onfocus="if(this.value == 'Username') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Username';}" value="Username">
-		        		<input type="password" name="password" class="loginForm" onfocus="if(this.value == 'Password') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Password';}" value="Password"><br/>
-		        		<input type="submit" value="Login" name="login" class="minimal">
-		        		
-		        		<br/><br/>
-		        		
-		        		<?php if($wrong == 1){
-			        		echo "<font color='red'>Incorrect Username/Password</font>";
-		        		}
-		        		?>
-		        		
-		        	</form>
-		        	
-		        	<br/><br/>
-		        	Raspcontrol has been drastically improved! &middot <a href="https://github.com/Bioshox/Raspcontrol/blob/master/README.md" target="_blank">Find out more</a> 
-		        	
-	        	</center>
-	        	</div>
-	        	
-	       	</div>
-	       	<br/><br/><br/>
-	    </div>
-
-<?php    
-	} else {
-	require('_lib/includes/_header.php');
-?>
-
-	<div id="firstBlockContainer">
-	        <div class="firstBlockWrapper">
-	        	<strong>Raspcontrol Installation</strong>
-			<br/><br/>	
-				<center>Please choose a username and password to login with<br/><br/>
-		        	<form name="setup" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-		        		<input type="text" name="username" class="loginForm" onfocus="if(this.value == 'Username') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Username';}" value="Username">
-		        		<input type="password" name="password" class="loginForm" onfocus="if(this.value == 'Password') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Password';}" value="Password"><br/>
-		        		<input type="submit" value="Create Account" name="setup" class="minimal">
-		        		
-		        		
-		        		</center>
-				<br/><br/><br/><br/>
-				</form>
-			</div>
-	</div>
-	
+            <div id="firstBlockContainer">
+              <div class="firstBlockWrapper">
+                <div style="padding-top: 20px;">
+                  <center>
+                    Please login to Raspcontrol!<br/><br/>
+                    <form name="login" method="post" action="index.php">
+                      <input type="text" name="username" class="loginForm"
+                        onfocus="if (this.value == 'Username') {this.value = '';}"
+                        onblur="if (this.value == '') {this.value = 'Username';}" value="Username">
+                      <input type="password" name="password" class="loginForm"
+                        onfocus="if(this.value == 'Password') {this.value = '';}"
+                        onblur="if (this.value == '') {this.value = 'Password';}" value="Password"><br/>
+                      <input type="submit" value="Login" name="login" class="minimal">
+                      <br/><br/>
 <?php
+                      if($wrong == 1)
+                      {
+                          echo "<font color='red'>Incorrect Username/Password</font>";
+                      }
+?>
+                    </form>
+                    <br/><br/>
+                    Raspcontrol has been drastically improved! &middot <a href="https://github.com/Bioshox/Raspcontrol/blob/master/README.md" target="_blank">Find out more</a> 
+                  </center>
+       	        </div>
+              </div>
+              <br/><br/><br/>
+	    </div>
+<?php    
 	}
+        else
+        {
+            require('_lib/includes/_header.php');
+?>
+
+            <div id="firstBlockContainer">
+              <div class="firstBlockWrapper">
+                <strong>Raspcontrol Installation</strong>
+                <br/><br/>	
+                <center>
+                    Please choose a username and password to login with<br/><br/>
+                    <form name="setup" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                      <input type="text" name="username" class="loginForm" 
+                        onfocus="if(this.value == 'Username') {this.value = '';}"
+                        onblur="if (this.value == '') {this.value = 'Username';}" value="Username">
+                      <input type="password" name="password" class="loginForm"
+                        onfocus="if(this.value == 'Password') {this.value = '';}"
+                        onblur="if (this.value == '') {this.value = 'Password';}" value="Password"><br/>
+                      <input type="submit" value="Create Account" name="setup" class="minimal">
+                </center>
+                <br/><br/><br/><br/>
+                </form>
+              </div>
+            </div>
+<?php
+        }
 }
 
 require('_lib/includes/_footer.php'); 
--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
+// This file is for users who don't use the PHP Server
 
-	// This file is for users who don't use the PHP Server
-	
-	//Mobile detection
-	include 'app/mobile/Mobile_Detect.php';
-	$detect = new Mobile_Detect();
-	if ($detect->isMobile() || $detect->isTablet()) {
-		header('Location: app/mobile');
-	}
-	else {
-		header('Location: app');
-	}
+// Mobile detection
+include 'app/mobile/Mobile_Detect.php';
+
+$detect = new Mobile_Detect();
+
+if ($detect->isMobile() || $detect->isTablet())
+{
+    header('Location: app/mobile');
+}
+else
+{
+    header('Location: app');
+}
+?>
